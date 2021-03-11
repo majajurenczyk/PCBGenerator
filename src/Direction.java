863ca24d70fa3a63a9cda1200bda250683c802c5
majajurenczyk <majka.jurenczyk@gmail.com>
2021-03-11 15:15:31 +0100

    Directions added

---
+++ src/Direction.java
@@ -0,0 +1,8 @@
+public class Direction {
+    public static final int NO_DIRECTION = 0;
+    public static final int HORIZONTAL_LEFT = 1;
+    public static final int HORIZONTAL_RIGHT = 2;
+    public static final int VERTICAL_UP = 3;
+    public static final int VERTICAL_DOWN = 4;
+}
+
